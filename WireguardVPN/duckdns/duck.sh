438c46638b8d808c702778f81e464d091869b88a
conorstew <71521539+conorstew@users.noreply.github.com>
2021-06-11 19:33:45 -0700

    Update duck.sh

--- WireguardVPN/duckdns/duck.sh
+++ WireguardVPN/duckdns/duck.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=conorscoots&token=cea65abd-22b5-4b0a-9bda-cc6ebe823a9a&ip=" | curl -k -o ~/duckdns/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=blah blah your domain and token blah &ip=" | curl -k -o ~/duckdns/duck.log -K -
